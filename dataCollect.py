3681db14533b2aa763bd9638c575c688f07535f1
Olsonikk <44727515+Olsonikk@users.noreply.github.com>
2026-01-03 14:50:45 +0100

    New dataset (#2)
    * add new data samples
    
    * add new lumos dataset
    
    * Add new avada samples
    
    * Add new wingardium samples
    
    * Add new alohomora samples
    
    * Add requirements for python venv
    
    * Update dataCollect.py script
    
    * Update ML_CNN
    
    * Delete unused data
    
    * Repo cleanup
    
    * Delete dataCollect code since its updated version is on MagicWand-tools repo
    
    * update .gitignore
    
    * Add more data to dataset
    
    * add mysterious spell
    
    * remove old dataset
    
    * update notebook for new dataset
    
    * Update ML_CNN
    
    * Update ML random forest notebook

--- dataCollect.py
+++ dataCollect.py
@@ -1,41 +1,54 @@
-import serial  # Teraz nie ma konfliktu z nazwą pliku
+import serial
 import time
 import csv
+import os
+import sys
 
-# Ustawienia portu szeregowego
-port = "COM5"  # Ustaw odpowiedni port szeregowy (dla systemu Windows COMx, dla Linux /dev/ttyUSBx)
-baudrate = 9600  # Prędkość transmisji
+port = "COM8"  # Change to your serial port
+baudrate = 115200
 
+if len(sys.argv) > 1:
+    folder_name = sys.argv[1]
+else:
+    folder_name = input("Choose folder name: ")
+
+# Create folder if it doesn't exist
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
 fields = ['time', 'acc_X', 'acc_Y', 'acc_Z', 'gyro_X', 'gyro_Y', 'gyro_Z']
 
-# Otwieramy port szeregowy
 ser = serial.Serial(port, baudrate, timeout=1)
 time.sleep(1)
-count=0
+count = 0
 
 while(count < 30):
-    received_data = []
-    # Wysłanie znaku "x"
+    data_rows = []
+    
+    while len(data_rows) == 0:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').strip()
+            if line:
+                rows_data = line.split(';')
+                for row_str in rows_data:
+                    row_str = row_str.strip()
+                    if row_str:
+                        row = eval(f"[{row_str}]")
+                        data_rows.append(row)
+        else:
+            time.sleep(0.01)
 
-    # Czekamy chwilę, aby odbiorca miał czas na odpowiedź
-
-    # Odczytanie danych z portu szeregowego
-    if ser.in_waiting > 0:
-        received_data = ser.readline().decode('utf-8').strip()  # Odczytujemy dane i dekodujemy je
-        received_data = eval(f"[{received_data}]")
-        #print(f"Odczytano: {received_data}")
-    if len(received_data) == 0:
+    if len(data_rows) == 0:
         continue
 
-    count += 1
-    file_path = f"avada2/avada{count:03}.csv"
+    file_path = f"{folder_name}/{folder_name}{count:04d}.csv"
 
     with open(file_path, 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
-        csvwriter.writerows(received_data)
+        csvwriter.writerows(data_rows)
         
     print(f'New data appended to CSV file {file_path}')
+    count += 1
 
 ser.close()
